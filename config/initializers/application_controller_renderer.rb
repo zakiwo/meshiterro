c8dd8c39b914392e32124a821f9ce951db78d40c
zakiwo <hiroki.ozaki39@gmail.com>
2021-07-06 06:43:40 +0000

    addPostImage

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2185d767b72e27700ef98fae281d2c93e431871644d54b36d498608ab7cd6539eb1d91d54098f42c93cf8c2d0ab134fac464232689b35315bcf2670aabe17e49'
