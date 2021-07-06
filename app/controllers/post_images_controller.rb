be6059e7c271fbe0d7b4edd9c61d831a05ed85f1
zakiwo <hiroki.ozaki39@gmail.com>
2021-07-06 09:37:34 +0000

    addFavorites

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -3,6 +3,7 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new
   end
 
+
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
@@ -11,11 +12,12 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page]).reverse_order
   end
 
   def show
     @post_image =PostImage.find(params[:id])
+    @post_comment = PostComment
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:'homes#top'
-  resources :post_images, only:[:new, :create, :index, :show, :destroy]
+  resources :post_images, only:[:new, :create, :index, :show, :destroy] do
+    resource :favorites, only:[:create, :destroy]
+    
+    resources :post_comments, only:[:create, :destroy]
+  end
 end
+
