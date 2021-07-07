9029d5a50fae542f82fd81ef07286cfd59927afc
zakiwo <hiroki.ozaki39@gmail.com>
2021-07-07 05:53:51 +0000

    [finish]4-9

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to:'homes#top'
   resources :post_images, only:[:new, :create, :index, :show, :destroy] do
     resource :favorites, only:[:create, :destroy]
-    
+
     resources :post_comments, only:[:create, :destroy]
   end
+  resources :users, only:[:show, :edit, :update]
 end
 
